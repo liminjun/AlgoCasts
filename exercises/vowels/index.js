8e324b389933ae0e0f9125b4223c286bd1ed2c4e
liminjun <v-leeli@hotmail.com>
2020-09-24 21:30:25 +0800

    feat:find the vowels.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,36 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   var newStr = str.split("");
+//   let total = 0;
+//   for (var i = 0; i < newStr.length; i++) {
+//     if (
+//       newStr[i].toLowerCase() === "a" ||
+//       newStr[i].toLowerCase() === "e" ||
+//       newStr[i].toLowerCase() === "i" ||
+//       newStr[i].toLowerCase() === "o" ||
+//       newStr[i].toLowerCase() === "u"
+//     ) {
+//       total += 1;
+//     }
+//   }
+//   return total;
+// }
+// function vowels(str) {
+//   let count = 0;
+//   const checker = "aeiou";
 
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 module.exports = vowels;
